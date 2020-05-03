21c76bb58d4f1eb64b4fc3c36545001a86b0289c
Pete Bunting <petebunting@mac.com>
2020-05-03 12:37:13 +0100

    Update with scripts to create and update eodd.

---
+++ run_west_africa_analysis/setup_eodd/update_setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif eoddsetup.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_west_africa/EODataDownConfig_psql.json --update
