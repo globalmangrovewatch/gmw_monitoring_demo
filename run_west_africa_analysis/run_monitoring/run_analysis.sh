55b2672a1846427274cdfbed543e399bd126a336
Pete Bunting <petebunting@mac.com>
2020-05-07 23:24:14 +0100

    Add web proxy to scripts.

--- run_west_africa_analysis/run_monitoring/run_analysis.sh
+++ run_west_africa_analysis/run_monitoring/run_analysis.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"
+export https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"
+
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif python gen_process_cmds.py --gen
 
 sh ./gen_exe_hpccmds.sh
