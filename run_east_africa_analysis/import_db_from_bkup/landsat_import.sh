e8b636029af1740e45577f9328c954545563948a
Pete Bunting <petebunting@mac.com>
2020-05-15 21:30:43 +0100

    Add scripts to drop and import the landsat db table.

---
+++ run_east_africa_analysis/import_db_from_bkup/landsat_import.sh
@@ -0,0 +1,4 @@
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddresetimgs.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s LandsatGOOG --cleartab
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddimportdb.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s LandsatGOOG -i /scratch/a.pfb/gmw_monitoring/db_bkup/bkup_ea_LandsatGOOG_20200515.json
+
