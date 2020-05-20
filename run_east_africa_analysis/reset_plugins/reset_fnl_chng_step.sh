649f5313a7082f1f0b91b2053c509d9ccc09b9e6
Pete Bunting <petebunting@mac.com>
2020-05-20 15:44:13 +0100

    Add script to reset plugin.

---
+++ run_east_africa_analysis/reset_plugins/reset_fnl_chng_step.sh
@@ -0,0 +1,1 @@
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddresetimgs.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s LandsatGOOG --usranalysis --plugin Sentinel1GMWChangeFnl Sentinel2GMWChangeFnl LandsatGMWChangeFnl
