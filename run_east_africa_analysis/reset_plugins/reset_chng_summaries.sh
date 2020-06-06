913cb17241ca741c9c2f116b0e221b4469b985e3
Pete Bunting <petebunting@mac.com>
2020-06-06 18:32:11 +0100

    Added script to reset the GenChngSummaryFeats plugin.

---
+++ run_east_africa_analysis/reset_plugins/reset_chng_summaries.sh
@@ -0,0 +1,5 @@
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddresetimgs.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s LandsatGOOG --usranalysis --plugin GenChngSummaryFeats
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddresetimgs.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s Sentinel1ASF --usranalysis --plugin GenChngSummaryFeats
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-py-dev.sif eoddresetimgs.py -c /scratch/a.pfb/gmw_monitoring/scripts/eodd_config_east_africa/EODataDownConfig_psql.json -s Sentinel2GOOG --usranalysis --plugin GenChngSummaryFeats
+
+
