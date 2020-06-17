f1d481b99e532b47338b391a0efd80d2c85e9286
Pete Bunting <petebunting@mac.com>
2020-06-17 17:40:36 +0100

    Update with plugin to upload results to google cloud.

--- eodd_plugins/gmw_upload_chng_outs.py
+++ eodd_plugins/gmw_upload_chng_outs.py
@@ -5,6 +5,7 @@ import rsgislib
 
 import logging
 import os
+import datetime
 
 def upload_to_google_bucket(file_to_upload, goog_cred, bucket_name, bucket_dir):
     """
@@ -32,7 +33,7 @@ logger = logging.getLogger(__name__)
 class UploadGMWChange(EODataDownUserAnalysis):
 
     def __init__(self):
-        usr_req_keys = ["goog_cred", "bucket_name", "bucket_vec_dir", "bucket_lut_dir"]
+        usr_req_keys = ["goog_cred", "bucket_name", "bucket_vec_dir", "bucket_lut_dir", "lcl_lut_dir"]
         EODataDownUserAnalysis.__init__(self, analysis_name='UploadToGoog', req_keys=usr_req_keys)
 
     def perform_analysis(self, scn_db_obj, sen_obj, plgin_objs):
@@ -43,23 +44,30 @@ class UploadGMWChange(EODataDownUserAnalysis):
         success = True
         out_dict = None
 
-        if 'CreateSummaryVecFeats' in plgin_objs:
-            if plgin_objs['CreateSummaryVecFeats'].Completed and plgin_objs['CreateSummaryVecFeats'].Outputs and plgin_objs['CreateSummaryVecFeats'].Success:
-                scn_chng_info = plgin_objs['CreateSummaryVecFeats'].ExtendedInfo
-
-                import pprint
-                pprint.pprint(scn_chng_info)
-                vec_files = scn_chng_info["out_chng_vec"]
-                lut_files = scn_chng_info["out_vec_luts"]
-
-                for vec_file in vec_files:
-                    upload_to_google_bucket(vec_file, self.params["goog_cred"], self.params["bucket_name"], self.params["bucket_vec_dir"])
-
-                for lut_file in lut_files:
-                    upload_to_google_bucket(lut_file, self.params["goog_cred"], self.params["bucket_name"], self.params["bucket_lut_dir"])
-
+        if sen_obj.get_sensor_name() == 'LandsatGOOG':
+            scn_obs_date = scn_db_obj.Sensing_Time
+        elif sen_obj.get_sensor_name() == 'Sentinel2GOOG':
+            scn_obs_date = scn_db_obj.Sensing_Time
+        elif sen_obj.get_sensor_name() == 'Sentinel1ASF':
+            scn_obs_date = scn_db_obj.Acquisition_Date
         else:
-            logger.debug("No change features available as outputs from previous steps...")
+            raise Exception("Did not recognise the sensor name...")
+
+        base_date = datetime.datetime(year=2020, month=1, day=1)
+        base_date_delta = scn_obs_date - base_date
+        zero_date_delta = datetime.timedelta()
+        if base_date_delta > zero_date_delta:
+            if 'CreateSummaryVecFeats' in plgin_objs:
+                if plgin_objs['CreateSummaryVecFeats'].Completed and plgin_objs['CreateSummaryVecFeats'].Outputs and plgin_objs['CreateSummaryVecFeats'].Success:
+                    scn_chng_info = plgin_objs['CreateSummaryVecFeats'].ExtendedInfo
+
+                    for tile in scn_chng_info:
+                        upload_to_google_bucket(scn_chng_info[tile]['file'], self.params["goog_cred"], self.params["bucket_name"], self.params["bucket_vec_dir"])
+                        lut_file_name = "gmw_{}_lut.json".format(tile)
+                        lut_file_path = os.path.join(self.params["lcl_lut_dir"], lut_file_name)
+                        upload_to_google_bucket(lut_file_path, self.params["goog_cred"], self.params["bucket_name"], self.params["bucket_lut_dir"])
+            else:
+                logger.debug("No change features available as outputs from previous steps...")
 
         return success, out_dict, False
 
