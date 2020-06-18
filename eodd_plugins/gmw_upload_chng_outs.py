07f40d922cc58c63dd9231898d3ff8544262b79d
Pete Bunting <petebunting@mac.com>
2020-06-18 11:50:52 +0100

    plugin update.

--- eodd_plugins/gmw_upload_chng_outs.py
+++ eodd_plugins/gmw_upload_chng_outs.py
@@ -39,22 +39,19 @@ def upload_to_google_bucket(file_to_upload, goog_cred, bucket_name, bucket_dir):
     :param bucket_dir: The directory path within the bucket for the file to be saved.
 
     """
+    from google.cloud import storage
     lcl_path, file_name = os.path.split(file_to_upload)
     bucket_file_path = os.path.join(bucket_dir, file_name)
-    print(list_files(goog_cred, bucket_name, bucket_dir))
-    if not blob_exists(goog_cred, bucket_name, bucket_file_path):
-        from google.cloud import storage
-        logger.debug("Uploading: {}".format(file_to_upload))
-        logger.debug("Using Google Credentials file: {}".format(goog_cred))
-    
-        # Explicitly use service account credentials by specifying the private key  file.
-        storage_client = storage.Client.from_service_account_json(goog_cred)
+    logger.debug("Uploading: {}".format(file_to_upload))
 
-        bucket_obj = storage_client.get_bucket(bucket_name)
-        blob_obj = bucket_obj.blob(bucket_file_path)
-        blob_obj.upload_from_filename(file_to_upload)
-        if not blob_exists(goog_cred, bucket_name, bucket_file_path):
-            raise Exception("File was not successfully uploaded to Google Cloud: '{}'".format(file_to_upload))
+    # Explicitly use service account credentials by specifying the private key  file.
+    storage_client = storage.Client.from_service_account_json(goog_cred)
+
+    bucket_obj = storage_client.get_bucket(bucket_name)
+    blob_obj = bucket_obj.blob(bucket_file_path)
+    blob_obj.upload_from_filename(file_to_upload)
+    if not blob_exists(goog_cred, bucket_name, bucket_file_path):
+        raise Exception("File was not successfully uploaded to Google Cloud: '{}'".format(file_to_upload))
 
 
 class UploadGMWChange(EODataDownUserAnalysis):
