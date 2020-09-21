2d24a4be72cd1d0d8281b3cee6b1f0aa24c208ba
Pete Bunting <petebunting@mac.com>
2020-09-21 23:26:30 +0100

    Add scripts to merge tiles outputs

---
+++ run_east_africa_analysis/create_bin_change_prod/02_merge_scn_tiles/gen_cmds.py
@@ -0,0 +1,50 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import os.path
+import logging
+import glob
+
+logger = logging.getLogger(__name__)
+
+class GenCmdsGenBinChngMsks(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        import rsgislib
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        tiles = glob.glob(os.path.join(kwargs['ref_tiles_path'], "*.kea"))
+        for tile in tiles:
+            tile_basename = rsgis_utils.get_file_basename(tile).replace("_chng_scr", "").replace("gmw_", "")
+            tiles_to_merge = glob.glob(os.path.join(kwargs['scn_wgs84_img_path'], "*", "*{}*.kea".format(tile_basename)))
+            if len(tiles_to_merge) > 0:
+                c_dict = dict()
+                c_dict["merge_imgs"] = tiles_to_merge
+                c_dict["out_img"] = os.path.join(kwargs['out_tile_path'], "{}_chng_bin_msk.kea".format(tile_basename))
+                c_dict["out_vec_file"] = os.path.join(kwargs['out_tile_path'], "{}_chng_bin_msk.gpkg".format(tile_basename))
+                c_dict["out_vec_lyr"] = tile_basename
+                if not (os.path.exists(c_dict["out_vec_file"]) and os.path.exists(c_dict["out_vec_file"])):
+                    self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(scn_wgs84_img_path='/scratch/a.pfb/gmw_monitoring/data_out/gmw_scn_bin_chng_wgs84_tiles',
+                              ref_tiles_path='/scratch/a.pfb/gmw_monitoring/monitoring/scores_imgs',
+                              out_tile_path='/scratch/a.pfb/gmw_monitoring/data_out/gmw_bin_chng_wgs84_tiles',
+                              out_vec_tile_path='/scratch/a.pfb/gmw_monitoring/data_out/gmw_bin_chng_wgs84_tiles_vec')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("merge_tile_chng_msks", 18504, '/scratch/a.pfb/gmw_monitoring/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10, n_xtr_cmds=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("merge_bin_tiles.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'merge_bin_tiles'
+    process_tools_cls = 'MergeBinChngMsk'
+
+    create_tools = GenCmdsGenBinChngMsks(cmd=script_cmd, db_conn_file="/home/a.pfb/eodd_gmw_info/pbpt_db_conn_east.txt",
+                                        lock_file_path="./gmw_monitor_lock_file.txt",
+                                        process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
+
---
+++ run_east_africa_analysis/create_bin_change_prod/02_merge_scn_tiles/merge_bin_tiles.py
@@ -0,0 +1,40 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.vectorutils
+import rsgislib.imageutils.imagelut
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+class MergeBinChngMsk(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='exe_scn_processing.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.imagecalc.calcMultiImgBandStats(self.params['merge_imgs'], self.params['out_img'], rsgislib.SUMTYPE_MAX, 'KEA', rsgislib.TYPE_8UINT, 0, False)
+        rsgislib.vectorutils.polygoniseRaster2VecLyr(self.params['out_vec_file'], self.params['out_vec_lyr'], 'GPKG', self.params['out_img'], imgBandNo=1, maskImg=self.params['out_img'],
+                                                     imgMaskBandNo=1, replace_file=True, replace_lyr=True, pxl_val_fieldname='PXLVAL')
+
+
+    def required_fields(self, **kwargs):
+        return ["merge_imgs", "out_img", "out_vec_file", "out_vec_lyr"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_img']] = 'gdal_image'
+        files_dict[self.params['out_vec_file']] = 'gdal_vector'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params['out_img']):
+            os.remove(self.params['out_img'])
+        if os.path.exists(self.params['out_vec_file']):
+            os.remove(self.params['out_vec_file'])
+
+if __name__ == "__main__":
+    MergeBinChngMsk().std_run()
+
+
