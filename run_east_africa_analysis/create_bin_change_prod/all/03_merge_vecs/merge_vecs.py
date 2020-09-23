9677cb1685ed0476d7603b5921996ea5b59d6265
Pete Bunting <petebunting@mac.com>
2020-09-23 21:05:39 +0100

    Bug fix.

--- run_east_africa_analysis/create_bin_change_prod/all/03_merge_vecs/merge_vecs.py
+++ run_east_africa_analysis/create_bin_change_prod/all/03_merge_vecs/merge_vecs.py
@@ -1,7 +1,7 @@
 import rsgislib.vectorutils
 import glob
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_monitoring/data_out/gmw_bin_chng_wgs84_tiles_vec/*.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_monitoring/data_out/gmw_bin_chng_wgs84_tiles/*.gpkg")
 
 #rsgislib.vectorutils.merge_vector_files(input_vecs, '/scratch/a.pfb/gmw_monitoring/data_out/gmw_opt_chng_feats_msk.gpkg', output_lyr='chng_feats', out_format='GPKG', out_epsg=None)
 
