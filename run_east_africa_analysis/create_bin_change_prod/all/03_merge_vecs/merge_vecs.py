52f7145cef98e456a70ac408591d1460dd93ad93
Pete Bunting <petebunting@mac.com>
2020-09-23 21:00:14 +0100

    Update using a different function to merge files.

--- run_east_africa_analysis/create_bin_change_prod/all/03_merge_vecs/merge_vecs.py
+++ run_east_africa_analysis/create_bin_change_prod/all/03_merge_vecs/merge_vecs.py
@@ -3,4 +3,6 @@ import glob
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_monitoring/data_out/gmw_bin_chng_wgs84_tiles_vec/*.gpkg")
 
-rsgislib.vectorutils.merge_vector_files(input_vecs, '/scratch/a.pfb/gmw_monitoring/data_out/gmw_opt_chng_feats_msk.gpkg', output_lyr='chng_feats', out_format='GPKG', out_epsg=None)
+#rsgislib.vectorutils.merge_vector_files(input_vecs, '/scratch/a.pfb/gmw_monitoring/data_out/gmw_opt_chng_feats_msk.gpkg', output_lyr='chng_feats', out_format='GPKG', out_epsg=None)
+
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, '/scratch/a.pfb/gmw_monitoring/data_out/gmw_opt_chng_feats_msk_v2.gpkg', 'chng_feats', False)
