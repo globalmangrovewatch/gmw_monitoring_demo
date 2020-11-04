a38630652281294f3f543c6a7dcc056164da7d3e
Pete Bunting <petebunting@mac.com>
2020-11-04 09:35:15 +0000

    Update.

--- run_west_africa_analysis/run_monitoring/gen_process_cmds.py
+++ run_west_africa_analysis/run_monitoring/gen_process_cmds.py
@@ -9,7 +9,7 @@ class CreateEODataDownCmds(PBPTGenQProcessToolCmds):
 
     def gen_command_info(self, **kwargs):
         import eodatadown.eodatadownrun
-        #eodatadown.eodatadownrun.find_new_downloads(kwargs['config_file'], kwargs['sensors'])
+        eodatadown.eodatadownrun.find_new_downloads(kwargs['config_file'], kwargs['sensors'])
 
         start_date = datetime.datetime(year=2020, month=7, day=1)
         end_date = datetime.datetime(year=2020, month=9, day=24)
